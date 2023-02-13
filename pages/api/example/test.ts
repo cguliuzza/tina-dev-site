2b509564a2366e25d7531b26f9d4a2e6e84999dd
Tina Guliuzza <christina.guliuzza@gmail.com>
2023-02-13 14:58:21 -0800

    up to me to define the request method

--- pages/api/example/test.ts
+++ pages/api/example/test.ts
@@ -1,3 +1,7 @@
 export default function handler(req, res) {
+  if (req.method !== 'GET') {
+    res.status(405).json({ error: 'Method not allowed' })
+    return res.end()
+  }
   res.status(200).json({ data: { name: 'Simba' } })
 }
