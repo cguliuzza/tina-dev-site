94a477d06d5a4a9f43bcfcc3299a49ae31632252
Tina Guliuzza <christina.guliuzza@gmail.com>
2023-02-14 10:10:31 -0800

    simplify code

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
+import styles from '../styles/home.module.css'
+
 export default async function Home() {
-  return <div>hello</div>
+  return (
+    <div className={styles.home}>
+      <div>
+        <div>name</div>
+        <div>blog</div>
+        <div>contact</div>
+      </div>
+    </div>
+  )
 }
