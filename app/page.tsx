576bd7f0f24bb8be3fd0bccaeff7b4640a9ff17a
Tina Guliuzza <christina.guliuzza@gmail.com>
2023-02-24 21:37:46 -0800

    install tailwindcss

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
     <div>
-      <h1>React Product Engineer</h1>
+      <h1 className="text-3xl font-bold underline">React Product Engineer</h1>
       <h2>Christina Guliuzza</h2>
     </div>
   )
