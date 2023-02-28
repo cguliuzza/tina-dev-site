b3bdf7a0ba87926ba676c5a39836e6371001e01b
Tina Guliuzza <christina.guliuzza@gmail.com>
2023-02-28 00:51:07 -0800

    navbar has been added to the template file and will show on all pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-import Header from '../components/Header'
 import Hero from '../components/Hero'
 import Footer from '../components/Footer'
 
 export default function Page() {
   return (
     <div>
-      <Header />
       <Hero />
       <Footer />
     </div>
