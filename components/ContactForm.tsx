5294defc7a2354d0c72463eb8a214de8413a3acf
Tina Guliuzza <christina.guliuzza@gmail.com>
2023-02-27 22:46:27 -0800

    require contact input fields, remove phone number field

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -94,6 +94,7 @@ export default function ContactForm() {
                 </label>
                 <div className="mt-2.5">
                   <input
+                    required={true}
                     type="text"
                     name="first-name"
                     id="first-name"
@@ -111,6 +112,7 @@ export default function ContactForm() {
                 </label>
                 <div className="mt-2.5">
                   <input
+                    required={true}
                     type="text"
                     name="last-name"
                     id="last-name"
@@ -128,27 +130,11 @@ export default function ContactForm() {
                 </label>
                 <div className="mt-2.5">
                   <input
+                    required={true}
                     type="email"
                     name="email"
                     id="email"
                     autoComplete="email"
-                    className="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-              <div className="sm:col-span-2">
-                <label
-                  htmlFor="phone-number"
-                  className="block text-sm font-semibold leading-6 text-gray-900"
-                >
-                  Phone number
-                </label>
-                <div className="mt-2.5">
-                  <input
-                    type="tel"
-                    name="phone-number"
-                    id="phone-number"
-                    autoComplete="tel"
                     className="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                 </div>
@@ -162,6 +148,7 @@ export default function ContactForm() {
                 </label>
                 <div className="mt-2.5">
                   <textarea
+                    required={true}
                     name="message"
                     id="message"
                     rows={4}
