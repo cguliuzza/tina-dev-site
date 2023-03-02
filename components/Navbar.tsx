70cbb2ffce2d9366196f8604d1ca3a1fc2790ff6
Tina Guliuzza <christina.guliuzza@gmail.com>
2023-03-02 13:00:47 -0800

    mobile - remove image from navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -89,13 +89,11 @@ export default function Navbar() {
         <div className="fixed inset-0 z-10" />
         <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center gap-x-6">
-            <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
-              <img
-                className="h-8 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                alt=""
-              />
+            <a href="/" className="-m-1.5 p-1.5">
+              <span className="sr-only">
+                Christina Guliuzza | React Product Engineer
+              </span>
+              <HomeModernIcon className="h-8 w-auto" />
             </a>
             <a
               href="/contact"
