2bec29d6228f4b0b29610e9023b0491a69719910
Tina Guliuzza <christina.guliuzza@gmail.com>
2023-03-13 13:46:28 -0700

    fix mobile nav icon alignment and clicking contact button will close mobile menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -32,7 +32,7 @@ export default function Navbar() {
   return (
     <header className="bg-white border-b border-solid border-slate-500/10">
       <nav
-        className="mx-auto flex max-w-7xl items-center justify-between gap-x-6 lg:px-8"
+        className="mx-auto flex max-w-7xl items-center justify-between gap-x-6 px-6 lg:px-8"
         aria-label="Global"
       >
         <div className="hidden lg:flex lg:gap-x-12">
@@ -56,9 +56,9 @@ export default function Navbar() {
             Contact Me
           </a>
         </div>
-        <div>
+        <div className="flex">
           <Image
-            className="relative w-20 h-20 pt-2 object-top object-cover overflow-hidden hover:{showHello}"
+            className="relative w-20 h-20 pt-2 object-top object-cover overflow-hidden"
             src={`/images/tinabgremoved.png`}
             alt=""
             width={180}
@@ -68,7 +68,7 @@ export default function Navbar() {
         <div className="flex lg:hidden">
           <button
             type="button"
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 px-6 text-slate-700"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-slate-700"
             onClick={() => setMobileMenuOpen(true)}
           >
             <span className="sr-only">Open main menu</span>
@@ -87,13 +87,14 @@ export default function Navbar() {
           <div className="flex items-center gap-x-6">
             <a
               href="/#contact"
+              onClick={() => setMobileMenuOpen(false)}
               className="ml-auto rounded-md bg-cyan-600 py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-cyan-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-600"
             >
               Contact Me
             </a>
             <button
               type="button"
-              className="-m-2.5 rounded-md p-3 text-slate-700"
+              className="-m-2.5 rounded-md p-2.5 text-slate-700"
               onClick={() => setMobileMenuOpen(false)}
             >
               <span className="sr-only">Close menu</span>
