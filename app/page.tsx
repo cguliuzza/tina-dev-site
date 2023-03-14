ca31dd61b2cf39c95cf60abf0e483e3ad6593e92
Tina Guliuzza <christina.guliuzza@gmail.com>
2023-03-13 22:23:55 -0700

    remove contact form from site

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import Hero from '../components/Hero'
 import Testimonials from '../components/Testimonials'
 import About from '../components/About'
-import ContactForm from '../components/ContactForm'
 
 export default function Page() {
   return (
@@ -9,7 +8,6 @@ export default function Page() {
       <Hero />
       <Testimonials />
       <About />
-      <ContactForm />
     </div>
   )
 }
