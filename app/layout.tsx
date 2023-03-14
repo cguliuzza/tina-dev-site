36516ce7c24aefe9816d3c246b3c9cc02ea713de
Tina Guliuzza <christina.guliuzza@gmail.com>
2023-03-13 22:29:33 -0700

    remove head file and move metadata to layout file

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,12 @@ import '../styles/global.css'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 
+export const metadata = {
+  title: 'Christina Guliuzza | React Product Engineer',
+  description:
+    'Hey, I’m Tina, I’m a likeable product and user-focused React Engineer.',
+}
+
 export default function RootLayout({
   children,
 }: {
@@ -9,6 +15,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+      </head>
       <body>
         <Header />
         {children}
