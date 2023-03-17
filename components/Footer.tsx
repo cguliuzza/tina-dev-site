be38566b09539fbea191ca7f891fd64104d34d60
Tina Guliuzza <christina.guliuzza@gmail.com>
2023-03-17 08:52:19 -0700

    change order of links and make them a column in mobile

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -24,18 +24,13 @@ const navigation = [
 export default function Footer() {
   return (
     <footer id="contact" className="bg-white">
-      <div className="mx-auto max-w-7xl overflow-hidden py-20 px-6 sm:py-24 lg:px-8">
-        <nav className="flex items-center justify-center" aria-label="Footer">
-          <div className="p-6">
-            <a
-              href="mailto:hello@christinaguliuzza.com"
-              className="text-center text-sm leading-6 text-slate-500 hover:text-slate-900"
-            >
-              hello@christinaguliuzza.com
-            </a>
-          </div>
+      <div className="mx-auto max-w-2xl py-24 px-6 lg:py-10">
+        <nav
+          className="flex flex-col md:flex-row items-center justify-center"
+          aria-label="Footer"
+        >
           {navigation.map((item) => (
-            <div key={item.name} className="p-6">
+            <div key={item.name} className="p-4">
               <a
                 href={item.href}
                 className="text-sm leading-6 text-slate-500 hover:text-slate-900"
@@ -44,6 +39,14 @@ export default function Footer() {
               </a>
             </div>
           ))}
+          <div className="p-4">
+            <a
+              href="mailto:hello@christinaguliuzza.com"
+              className="text-center text-sm leading-6 text-slate-500 hover:text-slate-900"
+            >
+              hello@christinaguliuzza.com
+            </a>
+          </div>
         </nav>
         <p className="mt-10 text-center text-xs leading-5 text-slate-400">
           &copy;<span>{currentYear}</span> Christina Guliuzza. All rights
