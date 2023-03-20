d4a7f77c09936acd97acd5669615c059e6c330ac
Tina Guliuzza <christina.guliuzza@gmail.com>
2023-03-19 20:18:16 -0700

    Fix - Type error: Type '{ enableBackground: string; }' is not assignable to type 'Properties<string | number, string & {}>'

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -51,7 +51,6 @@ export default function Navbar() {
                 x="0px"
                 y="0px"
                 viewBox="0 0 152.9 43.4"
-                style={{ enableBackground: 'new 0 0 152.9 43.4' }}
                 xmlSpace="preserve"
               >
                 <path d="M151.9,13.6c0,0,3.3-9.5-85-8.3c-97,1.3-58.3,29-58.3,29s9.7,8.1,69.7,8.1c68.3,0,69.3-23.1,69.3-23.1 s1.7-10.5-14.7-18.4" />
