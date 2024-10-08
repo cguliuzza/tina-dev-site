ec23f69349e3d421091fc0696f9b2dfa5c26ea9b
Tina Guliuzza <christina.guliuzza@gmail.com>
2024-10-08 08:28:48 -0700

    fix: show portfolio cards under hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from '../components/Hero'
 import Testimonials from '../components/Testimonials'
 import About from '../components/About'
+import PortfolioCards from '../components/PortfolioCards'
 
 export default function Page() {
   return (
     <div>
       <Hero />
+      <PortfolioCards />
       <Testimonials />
       <About />
     </div>
