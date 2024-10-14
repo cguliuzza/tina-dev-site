ce328d26b46233e240cfaec0308ed3e30cf7e50b
Tina Guliuzza <christina.guliuzza@gmail.com>
2024-10-14 15:40:07 -0700

    fix: remove image and add name to navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,6 @@
 import { useState } from 'react'
 import { Dialog } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
-import Image from 'next/image'
 
 const navigation = [
   {
@@ -30,12 +29,13 @@ export default function Navbar() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
-    <header className="relative z-50 bg-white border-b border-solid border-slate-500/10">
+    <header className="relative py-4 z-50 bg-white border-b border-solid border-slate-500/10">
       <nav
         className="mx-auto flex max-w-7xl items-center justify-between gap-x-6 px-6 lg:px-8"
         aria-label="Global"
       >
         <div className="menu hidden lg:flex lg:gap-x-12">
+          <span className='text-slate-500 font-bold'>Christina Guliuzza</span>
           {navigation.map((item) => (
             <a
               key={item.name}
@@ -64,31 +64,8 @@ export default function Navbar() {
             className="text-sm font-medium"
           >
             hello@christinaguliuzza.com{' '}
-            <span className="hidden sm:inline-block wave">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="w-6 h-6 inline"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M10.05 4.575a1.575 1.575 0 10-3.15 0v3m3.15-3v-1.5a1.575 1.575 0 013.15 0v1.5m-3.15 0l.075 5.925m3.075.75V4.575m0 0a1.575 1.575 0 013.15 0V15M6.9 7.575a1.575 1.575 0 10-3.15 0v8.175a6.75 6.75 0 006.75 6.75h2.018a5.25 5.25 0 003.712-1.538l1.732-1.732a5.25 5.25 0 001.538-3.712l.003-2.024a.668.668 0 01.198-.471 1.575 1.575 0 10-2.228-2.228 3.818 3.818 0 00-1.12 2.687M6.9 7.575V12m6.27 4.318A4.49 4.49 0 0116.35 15m.002 0h-.002"
-                />
-              </svg>
-            </span>
           </a>
         </button>
-        <Image
-          className="relative w-20 h-20 pt-2 object-top object-cover overflow-hidden"
-          src={`/images/tinabgremoved.png`}
-          alt=""
-          width={180}
-          height={37}
-        />
         <div className="flex lg:hidden">
           <button
             type="button"
@@ -107,7 +84,7 @@ export default function Navbar() {
         onClose={setMobileMenuOpen}
       >
         <div className="fixed inset-0 z-10" />
-        <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 pb-6 sm:max-w-sm sm:ring-1 sm:ring-slate-900/10">
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-4 sm:max-w-sm sm:ring-1 sm:ring-slate-900/10">
           <div className="flex items-center gap-x-6">
             <button className="group block ml-auto items-center justify-end rounded-full text-white text-opacity-90 bg-teal-900 py-2 px-6 shadow-sm hover:bg-opacity-90 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:bg-opacity-90">
               <a
@@ -115,33 +92,8 @@ export default function Navbar() {
                 className="text-sm font-medium"
               >
                 hello@christinaguliuzza.com{' '}
-                <span className="hidden sm:inline-block wave">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="currentColor"
-                    className="w-6 h-6 inline"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M10.05 4.575a1.575 1.575 0 10-3.15 0v3m3.15-3v-1.5a1.575 1.575 0 013.15 0v1.5m-3.15 0l.075 5.925m3.075.75V4.575m0 0a1.575 1.575 0 013.15 0V15M6.9 7.575a1.575 1.575 0 10-3.15 0v8.175a6.75 6.75 0 006.75 6.75h2.018a5.25 5.25 0 003.712-1.538l1.732-1.732a5.25 5.25 0 001.538-3.712l.003-2.024a.668.668 0 01.198-.471 1.575 1.575 0 10-2.228-2.228 3.818 3.818 0 00-1.12 2.687M6.9 7.575V12m6.27 4.318A4.49 4.49 0 0116.35 15m.002 0h-.002"
-                    />
-                  </svg>
-                </span>
               </a>
             </button>
-            <div className="flex">
-              <Image
-                className="relative w-20 h-20 pt-2 object-top object-cover overflow-hidden"
-                src={`/images/tinabgremoved.png`}
-                alt=""
-                width={180}
-                height={37}
-              />
-            </div>
             <button
               type="button"
               className="-m-2.5 rounded-md p-2.5 text-slate-700"
